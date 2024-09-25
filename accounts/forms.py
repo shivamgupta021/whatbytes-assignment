e939fc371eeb88127ce5da468297552b354c03b7
Shivam Gupta <shivam.gupta02210@gmail.com>
2024-09-25 15:25:59 +0530

    Add forms for user signup and login
    Signed-off-by: Shivam Gupta <shivam.gupta02210@gmail.com>

---
+++ accounts/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import User
+
+
+class UserSignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("username", "email")
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label="Username or Email")
